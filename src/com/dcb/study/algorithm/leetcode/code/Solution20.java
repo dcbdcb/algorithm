a7e912523f05aac81ac7323d90bb78387c373a95
dcb1989 <535260620@qq.com>
2018-08-20 23:44:59 +0800

    solution 20

--- src/com/dcb/study/algorithm/leetcode/code/Solution20.java
+++ src/com/dcb/study/algorithm/leetcode/code/Solution20.java
@@ -5,8 +5,6 @@ import java.util.Stack;
 public class Solution20 {
 
     public static void main(String[] args) {
-        int a = 161+70+113+131+200+299+98+151+117+281+89+128+276+70+161+100+82+95+221;
-        System.out.println(a);
         Solution20 s = new Solution20();
         System.out.println(s.isValid("asdfa{}"));
 
