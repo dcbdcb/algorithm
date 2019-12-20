bb8189fe8be1c8960725d25d7c027a8089bd5f80
dcb1989 <535260620@qq.com>
2019-12-20 23:29:55 +0800

    430

---
+++ src/com/dcb/study/algorithm/leetcode/code/Node.java
@@ -0,0 +1,21 @@
+package com.dcb.study.algorithm.leetcode.code;
+
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        if (this.next == null) {
+            return String.valueOf(this.val);
+        }
+        System.out.println(this.val);
+        return this.next.toString();
+    }
+}
