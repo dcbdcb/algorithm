b66309203de8f9684a1de34a063cc76f177667d3
dcb1989 <535260620@qq.com>
2019-12-21 22:19:48 +0800

    237

--- leetcode/src/main/java/solution/Solution237.java
+++ leetcode/src/main/java/solution/Solution237.java
@@ -2,6 +2,27 @@ package solution;
 
 public class Solution237 {
 
+    public static void main(String[] args) {
+        Solution237 s = new Solution237();
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        System.out.println(a);
+        s.deleteNode(c);
+        System.out.println(a);
+    }
+
+    /**
+     * 删除节点
+     *
+     * @param node 需要删除的节点
+     */
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
