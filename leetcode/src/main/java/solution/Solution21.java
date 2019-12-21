b38fabbdeb792f6846a6fd7321746bb5956b8008
dcb1989 <535260620@qq.com>
2019-12-21 23:52:10 +0800

    21

--- leetcode/src/main/java/solution/Solution21.java
+++ leetcode/src/main/java/solution/Solution21.java
@@ -1,25 +1,35 @@
-package com.dcb.study.algorithm.leetcode.code;
-
-import java.util.Arrays;
+package solution;
 
 public class Solution21 {
 
     public static void main(String[] args) {
-        int [] a = new int[]{1,2,3,4,5,6,7,8,9,10};
+        Solution21 s = new Solution21();
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(4);
+        a.next = b;
+        b.next = c;
 
-        print(Arrays.copyOfRange(a,0,5));
-        print(Arrays.copyOfRange(a,5,10));
+        ListNode x = new ListNode(1);
+        ListNode y = new ListNode(3);
+        ListNode z = new ListNode(4);
+        x.next = y;
+        y.next = z;
+        System.out.println(s.mergeTwoLists(a, x));
+
     }
 
-    public static void print(int[] a) {
-        for (int i : a) {
-            System.out.println(i);
-        }
-    }
-
+    /**
+     * 合并两个有序链表
+     *
+     * @param l1 链表1头节点
+     * @param l2 链表2头节点
+     * @return 合并后的链表头节点
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
+
         ListNode head = new ListNode(1);
         ListNode tmp = head;
         while (l1 != null && l2 != null) {
