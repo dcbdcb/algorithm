cd5e828da3502935b80b26abe10b773e1a384f26
dcb1989 <535260620@qq.com>
2019-12-29 16:36:21 +0800

    104

---
+++ leetcode/src/main/java/solution/Solution104.java
@@ -0,0 +1,30 @@
+package solution;
+
+public class Solution104 {
+
+    public static void main(String[] args) {
+        Solution104 s = new Solution104();
+        TreeNode a = new TreeNode(3);
+        TreeNode b = new TreeNode(9);
+        TreeNode c = new TreeNode(20);
+        TreeNode d = new TreeNode(15);
+        TreeNode e = new TreeNode(7);
+        a.left = b;
+        a.right = c;
+        c.left = d;
+        c.right = e;
+        System.out.println(s.maxDepth(a));
+    }
+
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    private int max(int a, int b) {
+        return a > b ? a : b;
+    }
+}
