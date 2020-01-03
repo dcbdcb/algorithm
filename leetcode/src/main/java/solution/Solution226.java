64aaa7f3168157593aae1b8cf8370d9550fea042
dcb1989 <535260620@qq.com>
2020-01-03 16:20:47 +0800

    226

---
+++ leetcode/src/main/java/solution/Solution226.java
@@ -0,0 +1,21 @@
+package solution;
+
+public class Solution226 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        TreeNode tempRight = invertTree(root.left);
+        TreeNode tempLeft = invertTree(root.right);
+        root.right = tempRight;
+        root.left = tempLeft;
+        return root;
+    }
+
+}
