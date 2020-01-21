42b99c56c13177359a86bbc8da466ef6c2f6ee94
dcb1989 <535260620@qq.com>
2020-01-21 17:19:41 +0800

    172

---
+++ leetcode/src/main/java/solution/Solution172.java
@@ -0,0 +1,29 @@
+package solution;
+
+public class Solution172 {
+
+
+    public static void main(String[] args) {
+        Solution172 s = new Solution172();
+
+        System.out.println(s.trailingZeroes(1808548329));
+    }
+
+    public int trailingZeroes(int n) {
+        int result = 0;
+        while (true) {
+
+            if (n / 5 > 0) {
+                result += n / 5;
+                n = n / 5;
+            } else {
+                return result;
+            }
+        }
+
+    }
+
+
+
+
+}
