2cd774ced0f15f931338997587688fe26cb84e09
dcb19 <535260620@qq.com>
2020-01-28 22:27:45 +0800

    724

--- leetcode/src/main/java/solution/Solution724.java
+++ leetcode/src/main/java/solution/Solution724.java
@@ -9,12 +9,14 @@ public class Solution724 {
     }
 
     public int pivotIndex(int[] nums) {
+        // 循环数组求和
         int sum = 0;
         for (Integer num : nums) {
             sum += num;
         }
 
         int leftSum = 0;
+        // 遍历数组，计算左边和是否等于右边和，如果相同则返回，不同计算下一位
         for (int i = 0; i < nums.length; i++) {
             if (leftSum == sum - leftSum - nums[i]) {
                 return i;
